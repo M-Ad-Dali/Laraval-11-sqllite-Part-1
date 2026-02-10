a7c8aa182d651a345f1be86112e0768a2d71785d
Moka-Ad-Dali <mohammed.addali@gmail.com>
2026-02-10 11:28:58 +0300

    add code db:seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Job;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,8 +19,11 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
+            'first_name' => 'Mohammed',
+            'last_name' => 'Al-Dali',
             'email' => 'test@example.com',
         ]);
+        
+        $this->call(JobSeeder::class);
     }
 }
